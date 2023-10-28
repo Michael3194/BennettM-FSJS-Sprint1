e3818baa235ee56c4f178db45c07509d58346a1b
Michael3194 <143271230+Michael3194@users.noreply.github.com>
2023-10-28 20:39:09 -0230

    comment

--- token.js
+++ token.js
@@ -280,7 +280,7 @@ function updateToken(myArgs) {
             })
         }
     })
-}
+} // End of updateToken()
 
 /* --------------------------------------------------- */
 /*                      tokenApp()                     */
